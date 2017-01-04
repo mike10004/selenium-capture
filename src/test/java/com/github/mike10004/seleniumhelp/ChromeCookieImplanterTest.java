c3ddd4d038d6b2f7694f1cf15f8a1993fa6c9cf8
Mike Chaberski <mchaberski@novetta.com>
2017-01-04 17:50:49 -0500

    add test confirming transform works if input has no expiry

--- src/test/java/com/github/mike10004/seleniumhelp/ChromeCookieImplanterTest.java
+++ src/test/java/com/github/mike10004/seleniumhelp/ChromeCookieImplanterTest.java
@@ -1,5 +1,6 @@
 package com.github.mike10004.seleniumhelp;
 
+import com.github.mike10004.seleniumhelp.ChromeWebDriverFactory.ChromeCookieTransform;
 import com.github.mike10004.seleniumhelp.ChromeWebDriverFactory.CookieImplanter;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
@@ -10,12 +11,14 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkState;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 
 public class ChromeCookieImplanterTest {
 
@@ -41,5 +44,36 @@ public class ChromeCookieImplanterTest {
         assertEquals("name", "foo", cookieObject.get("name").getAsString());
         assertEquals("value", "bar", cookieObject.get("value").getAsString());
         assertFalse("cookie has 'session' field but shouldn't", cookieObject.has("session"));
+    }
+
+    @Test
+    public void ChromeCookieTransform_transform_noExpiryDate() {
+        DeserializableCookie d = new DeserializableCookie();
+        d.setName("foo");
+        d.setValue("bar");
+        d.setDomain("example.com");
+        ChromeCookieTransform transform = new ChromeCookieTransform();
+        ChromeCookie c = transform.transform(d);
+        assertEquals("name", d.getName(), c.name);
+        assertEquals("value", d.getValue(), c.value);
+        assertEquals("domain", d.getBestDomainProperty(), c.domain);
+        assertNull("expirationDate", c.expirationDate);
+    }
+
+    @Test
+    public void ChromeCookieTransform_transform() {
+        DeserializableCookie d = new DeserializableCookie();
+        d.setName("foo");
+        d.setValue("bar");
+        d.setDomain("example.com");
+        Date expiryDate = new Date();
+        d.setExpiryDate(expiryDate);
+        ChromeCookieTransform transform = new ChromeCookieTransform();
+        ChromeCookie c = transform.transform(d);
+        assertEquals("name", d.getName(), c.name);
+        assertEquals("value", d.getValue(), c.value);
+        assertEquals("domain", d.getBestDomainProperty(), c.domain);
+        double expiryDateInSeconds = expiryDate.getTime() / 1000d;
+        assertEquals("expirationDate", expiryDateInSeconds, c.expirationDate.doubleValue(), 0.0001);
     }
 }
--- src/test/java/com/github/mike10004/seleniumhelp/CookieStorageTest.java
+++ src/test/java/com/github/mike10004/seleniumhelp/CookieStorageTest.java
@@ -1,22 +1,16 @@
 package com.github.mike10004.seleniumhelp;
 
-import com.github.mike10004.seleniumhelp.ChromeWebDriverFactory.CookiePreparer;
 import com.github.mike10004.xvfbtesting.XvfbRule;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-import io.github.bonigarcia.wdm.MarionetteDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import net.lightbody.bmp.core.har.Har;
 import org.apache.http.cookie.MalformedCookieException;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -32,7 +26,7 @@ public class CookieStorageTest {
 
     @Test
     public void testFirefox() throws Exception {
-        MarionetteDriverManager.getInstance().setup();
+        FirefoxDriverManager.getInstance().setup();
         testWithDriverFactory(new FirefoxWebDriverFactory(xvfb.getController().configureEnvironment(new HashMap<>()),
                 ImmutableMap.of(), ImmutableList.of()));
     }
@@ -50,7 +44,7 @@ public class CookieStorageTest {
         });
         Har har = collection.har;
         final FlexibleCookieSpec cookieSpec = FlexibleCookieSpec.getDefault();
-        List<DeserializableCookie> harCookies = HarAnalysis.of(har).findCookies(cookieSpec);
+        List<DeserializableCookie> harCookies = HarAnalysis.of(har).findCookies(cookieSpec).makeUltimateCookieList();
         assertEquals("num cookies", 1, harCookies.size());
         checkCookies(harCookies);
     }
