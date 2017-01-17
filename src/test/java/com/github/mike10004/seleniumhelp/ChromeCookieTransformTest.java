937e14dac53caa10d2c1bbc9c2d636e242f5e4d8
Mike Chaberski <mchaberski@novetta.com>
2017-01-17 13:42:51 -0500

    remove test that used local cookies.json file

--- src/test/java/com/github/mike10004/seleniumhelp/ChromeCookieTransformTest.java
+++ src/test/java/com/github/mike10004/seleniumhelp/ChromeCookieTransformTest.java
@@ -26,27 +26,4 @@ public class ChromeCookieTransformTest {
         ChromeCookie output = t.transform(input);
         assertEquals("chrome cookie URL should be https if input cookie is secure", "https", new URL(output.url).getProtocol());
     }
-
-    @Test
-    public void transform_deserializable() throws Exception {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String dcJson = "{\"name\":\"JSESSIONID\",\"attribs\":{\"path\":\"/\",\"domain\":\".www.linkedin.com\",\"max-age\":\"7776000\",\"version\":\"1\"},\"value\":\"ajax:6662599637803940570\",\"cookieDomain\":\"www.linkedin.com\",\"cookieExpiryDate\":\"Apr 16, 2017 5:52:11 PM\",\"cookiePath\":\"/\",\"isSecure\":true,\"cookieVersion\":0,\"creationDate\":\"Jan 16, 2017 4:51:57 PM\",\"httpOnly\":false}";
-        DeserializableCookie d = gson.fromJson(dcJson, DeserializableCookie.class);
-        ChromeCookieTransform t = new ChromeCookieTransform();
-        ChromeCookie c = t.transform(d);
-        gson.toJson(c, System.out);
-        System.out.println();
-
-        DeserializableCookie[] cookies;
-        try (java.io.Reader reader = new java.io.FileReader("/tmp/cookies.json")) {
-            cookies = gson.fromJson(reader, DeserializableCookie[].class);
-        }
-        List<ChromeCookie> ccookies = Stream.of(cookies).map(t::transform).collect(Collectors.toList());
-        File outfile = new File("/tmp/chrome-cookies.json");
-        try (java.io.Writer writer = new java.io.FileWriter(outfile)) {
-            gson.toJson(ccookies, writer);
-        }
-        System.out.format("%s written with ChromeCookie array%n", outfile);
-    }
-
 }
