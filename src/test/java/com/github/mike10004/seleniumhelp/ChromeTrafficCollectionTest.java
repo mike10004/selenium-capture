7c6be290a7a8af9121bf744f92a067faf1b4a2c1
Mike Chaberski <mchaberski@gmail.com>
2018-09-18 10:54:51 -0400

    move related unit test classes into the same file

---
+++ src/test/java/com/github/mike10004/seleniumhelp/ChromeTrafficCollectionTest.java
@@ -0,0 +1,88 @@
+package com.github.mike10004.seleniumhelp;
+
+import net.lightbody.bmp.core.har.HarContent;
+import net.lightbody.bmp.core.har.HarResponse;
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import java.net.URL;
+
+@RunWith(Enclosed.class)
+public class ChromeTrafficCollectionTest {
+
+    public static class HttpTest extends CollectionTestBase {
+
+        public HttpTest() {
+            super("http");
+        }
+
+        @BeforeClass
+        public static void setUpDriver() {
+            String driverPath = System.getProperty("webdriver.chrome.driver");
+            if (driverPath == null) {
+                UnitTests.setupRecommendedChromeDriver();
+            }
+        }
+
+        @Test
+        public void http() throws Exception {
+            String display = xvfb.getController().getDisplay();
+            WebDriverFactory webDriverFactory = ChromeWebDriverFactory.builder()
+                    .chromeOptions(UnitTests.createChromeOptions())
+                    .environment(ChromeWebDriverFactory.createEnvironmentSupplierForDisplay(display))
+                    .build();
+            testTrafficCollector(webDriverFactory);
+        }
+
+        @Test
+        public void http_headless() throws Exception {
+            Assume.assumeFalse("headless tests disabled", UnitTests.isHeadlessChromeTestsDisabled());
+            WebDriverFactory webDriverFactory = ChromeWebDriverFactory.builder()
+                    .chromeOptions(UnitTests.createChromeOptions())
+                    .headless()
+                    .build();
+            testTrafficCollector(webDriverFactory);
+        }
+
+        @Test
+        public void http_headless_brotli() throws Exception {
+            Assume.assumeFalse("headless tests disabled", UnitTests.isHeadlessChromeTestsDisabled());
+            WebDriverFactory webDriverFactory = ChromeWebDriverFactory.builder()
+                    .chromeOptions(UnitTests.createChromeOptions())
+                    .headless()
+                    .build();
+            HarResponse response = testTrafficCollector(webDriverFactory, new URL("http://httpbin.org/brotli"));
+            HarContent content = response.getContent();
+            System.out.println(content.getText());
+        }
+    }
+
+    public static class HttpsTest extends CollectionTestBase {
+
+        public HttpsTest() {
+            super("https");
+        }
+
+        @BeforeClass
+        public static void setUpDriver() {
+            String driverPath = System.getProperty("webdriver.chrome.driver");
+            if (driverPath == null) {
+                UnitTests.setupRecommendedChromeDriver();
+            }
+        }
+
+        @Test
+        public void https() throws Exception {
+            String display = xvfb.getController().getDisplay();
+            WebDriverFactory webDriverFactory = ChromeWebDriverFactory.builder()
+                    .chromeOptions(UnitTests.createChromeOptions())
+                    .environment(ChromeWebDriverFactory.createEnvironmentSupplierForDisplay(display))
+                    .build();
+            testTrafficCollector(webDriverFactory);
+        }
+
+    }
+}
