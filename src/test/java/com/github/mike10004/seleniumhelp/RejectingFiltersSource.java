3a38599a1bdee140fa8ad1f79cc61832c4628984
Mike Chaberski <mchaberski@gmail.com>
2019-10-01 17:25:04 -0400

    move RejectingFiltersSource to test sources

--- src/test/java/com/github/mike10004/seleniumhelp/RejectingFiltersSource.java
+++ src/test/java/com/github/mike10004/seleniumhelp/RejectingFiltersSource.java
@@ -10,8 +10,8 @@ import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import net.lightbody.bmp.filters.HttpsAwareFiltersAdapter;
 import org.littleshoot.proxy.HttpFilters;
-import org.littleshoot.proxy.HttpFiltersAdapter;
 import org.littleshoot.proxy.HttpFiltersSourceAdapter;
 
 import java.nio.charset.Charset;
@@ -24,12 +24,18 @@ public abstract class RejectingFiltersSource extends HttpFiltersSourceAdapter {
 
     @Override
     public HttpFilters filterRequest(HttpRequest originalRequest) {
+        return filterRequest(originalRequest, (ChannelHandlerContext) null);
+    }
+
+    @Override
+    public final HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) {
         requestCounter.incrementAndGet();
-        HttpFilters rejectingFilters = new HttpFiltersAdapter(originalRequest) {
+        HttpFilters rejectingFilters = new HttpsAwareFiltersAdapter(originalRequest, ctx) {
             @Override
             public HttpResponse clientToProxyRequest(HttpObject httpObject) {
-                if (isRejectTarget(originalRequest, httpObject)) {
-                    return createResponse(originalRequest, httpObject);
+                String fullUrl = getOriginalUrl();
+                if (isRejectTarget(originalRequest, fullUrl, httpObject)) {
+                    return createResponse(originalRequest, fullUrl, httpObject);
                 }
                 return null;
             }
@@ -37,19 +43,16 @@ public abstract class RejectingFiltersSource extends HttpFiltersSourceAdapter {
         return rejectingFilters;
     }
 
-    @Override
-    public final HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) {
-        return super.filterRequest(originalRequest, ctx);
-    }
+    protected abstract boolean isRejectTarget(HttpRequest originalRequest, String fullUrl, HttpObject httpObject);
 
-    protected abstract boolean isRejectTarget(HttpRequest originalRequest, HttpObject httpObject);
-
-    protected DefaultFullHttpResponse createResponse(HttpRequest originalRequest, HttpObject httpObject) {
+    protected DefaultFullHttpResponse createResponse(HttpRequest originalRequest, String fullUrl, HttpObject httpObject) {
         Charset charset = StandardCharsets.UTF_8;
-        ByteBuf content = Unpooled.wrappedBuffer(constructRejectionText(originalRequest, httpObject).getBytes(charset));
-        DefaultFullHttpResponse rejection = new DefaultFullHttpResponse(originalRequest.getProtocolVersion(),
+        byte[] byteArray = constructRejectionText(originalRequest, fullUrl, httpObject).getBytes(charset);
+        ByteBuf content = Unpooled.wrappedBuffer(byteArray);
+        DefaultFullHttpResponse rejection = new DefaultFullHttpResponse(originalRequest.protocolVersion(),
                 HttpResponseStatus.INTERNAL_SERVER_ERROR, content);
         rejection.headers().set(HttpHeaders.CONTENT_TYPE, MediaType.PLAIN_TEXT_UTF_8.withCharset(charset));
+        rejection.headers().set(HttpHeaders.CONTENT_LENGTH, byteArray.length);
         return rejection;
     }
 
@@ -57,7 +60,7 @@ public abstract class RejectingFiltersSource extends HttpFiltersSourceAdapter {
         return requestCounter.get();
     }
 
-    protected String constructRejectionText(HttpRequest originalRequest, HttpObject httpObject) {
+    protected String constructRejectionText(HttpRequest originalRequest, String fullUrl, HttpObject httpObject) {
         return "error";
     }
 }
