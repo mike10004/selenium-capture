b00ba60b0bf3d645cc028608b488e138a86e723b
Mike Chaberski <mchaberski@gmail.com>
2020-02-07 11:38:27 -0500

    remove javascript validator

--- src/test/java/com/github/mike10004/seleniumhelp/ReflectiveJdk8JavascriptValidatorTest.java
+++
@@ -1,8 +0,0 @@
-package com.github.mike10004.seleniumhelp;
-
-public class ReflectiveJdk8JavascriptValidatorTest extends JavascriptValidatorTestBase {
-    @Override
-    protected JavascriptValidator createValidator() {
-        return new ReflectiveJdk8JavascriptValidator();
-    }
-}
